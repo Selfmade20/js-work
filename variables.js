443d08fa73b682430d0fa85b4321079e2eed9668
Selfmade20e <tebogo1686@gmail.com>
2019-03-11 16:15:55 +0200

    added functions and variables

---
+++ variables.js
@@ -0,0 +1,62 @@
+function sayHelloTo(name, surname) {
+    // console.log(name, surname)
+    return name + " " + surname;
+}
+
+var name = "Tebza";
+var surname = "Selamolela";
+
+console.log(sayHelloTo(name, surname));
+
+function multiply(apples, juice){
+//console.log(apples, juice)
+return apples + juice;
+
+}
+
+var apples = 12
+var juice = 30
+
+var answer = apples + juice;
+console.log(multiply(apples, juice));
+
+
+function add(taxifare, food){
+//console.log(taxifare, food)
+return taxifare + food;
+
+}
+
+var taxifare = 180
+var food= 100
+
+var answer = taxifare + food;
+console.log(add(taxifare, food));
+
+
+
+function subtract(goats, sheeps){
+//console.log(goats, sheeps)
+return goats - sheeps;
+
+}
+
+var goats = 2600
+var sheeps= 1469
+
+//var answer = goats - sheeps;
+console.log(subtract(goats, sheeps));
+
+
+
+function multiply(people, cars){
+//console.log(people, cars)
+return people * cars;
+
+}
+
+var people = 30
+var cars = 10
+
+//var answer = people * cars;
+console.log(multiply(people, cars));
