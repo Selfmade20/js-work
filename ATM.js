554cf92f0961460f6c531e36512802409052c4ca
Selfmade20e <tebogo1686@gmail.com>
2019-04-11 15:45:36 +0200

    ATM with deposit function

--- ATM.js
+++ ATM.js
@@ -8,6 +8,7 @@ function giveUserBalance() {
     return "User Balance " + currentBalance();
 }
 
+
 function withdraw(withDrawalAmount) {
     var balance = currentBalance();
     if (balance >= withDrawalAmount) {
@@ -16,27 +17,50 @@ function withdraw(withDrawalAmount) {
     } else {
         return "Insufficient funds";
     }
+}
+
+function deposit(depositAmount) {
+    var balance = giveUserBalance();
+
+    if (depositAmount >= 15000) {
+        userBalance = userBalance + depositAmount;
+        return "Deposit Limit exceeded"
+    }
+
+    else {
+        userBalance = userBalance + depositAmount;
+        return "Deposit of " + depositAmount + " Successful";
+    }
+
 
 }
+
+
+
 console.log(giveUserBalance());
 console.log(withdraw(5000));
 console.log(giveUserBalance());
 console.log(withdraw(900));
 console.log(giveUserBalance());
-
-
-
-transactionHistory = [];
+console.log(withdraw(1200));
+console.log(giveUserBalance());
+console.log(deposit(10000));
+console.log(giveUserBalance());  
+var transactionHistory = [];
 
 transactionHistory.push(giveUserBalance());
 transactionHistory.push(withdraw(5000));
 transactionHistory.push(giveUserBalance());
 transactionHistory.push(withdraw(900));
 transactionHistory.push(giveUserBalance());
+transactionHistory.push(withdraw(1200));
+transactionHistory.push(giveUserBalance());
+transactionHistory.push(deposit(10000));
+transactionHistory.push(giveUserBalance());
 
 
 console.log(transactionHistory);
 
 function transactionHistory() {
- //console.log(transactionHistory)   
+    //console.log(transactionHistory)   
 }
