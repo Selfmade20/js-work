a95d99c955d4590f02107f604051b382e4baf2a7
Selfmade20 <tebogo1686@gmail.com>
2019-10-02 16:12:25 +0200

    Teller Machine

---
+++ Teller Machine.js
@@ -0,0 +1,61 @@
+const tellerMachine = {
+    listOfUsers: [],
+
+    addUser(users) {
+        console.log(this.listOfUsers);
+
+        this.listOfUsers.push(); // append (add) a default balance of 0.00 to every user object and append it to the the listOfusers.
+        for (var i = 0; i < users.lenth; i++) {
+
+            return this.listOfUsers;
+        }
+
+        depositIntoUserAccount({ pin, amount })
+        console.log(this.listOfUsers);
+
+        return amount + "has been deposited to my account" + name;
+          if (pin = tellerMachine)   //if pin is not equals to tellerMachine pin the transaction should not go through
+
+            return "sucessfull"
+
+    }
+};
+
+withDrawFromUserAccount({ pin, amount }) {
+    if (withDrawFromUserAccount >= amount);
+
+    return "insufficient funds"
+
+    console.log(this.listOfUsers);
+
+    console.log(withDrawFromUserAccount({ pin, amount }));
+
+    return amount + "has been credited to my account" + name
+
+    if (pin = tellermachine);  //if pin is not equals to tellerMachine pin the transaction should not go through
+    return "Sucessful"
+
+
+
+}
+
+
+};
+//for testing
+tellerMachine.addUser([
+    { name: "Sam", pin: "1234" },
+
+    { name: "Mary", pin: "0000" },
+
+    { name: "Stephen", pin: "1223" },
+
+    { name: "Lindiwe", pin: "0001" }
+
+]);
+
+tellerMachine.depositIntoUserAccount({ amount: 1233, pin: 1234 });
+
+tellerMachine.withDrawFromUserAccount({ amount: 1283, pin: 1234 });
+
+
+function deposit(depositAmount)
